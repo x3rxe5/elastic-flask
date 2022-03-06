8207afc693829244b4bb6dc068af6bc53ea2564c
xerxes <sharmapulkit310@gmail.com>
2022-03-06 21:11:35 +0530

    initial commit

--- api/retrieve_data.py
+++ api/retrieve_data.py
@@ -4,39 +4,40 @@ from flask import jsonify, request
 
 es = connect_elasticsearch()
 
+
 @app.route('/', methods=['GET'])
 def home():
     message = 'Flask is UP and RUNNING'
-    return jsonify(message),200
+    return jsonify(message), 200
 
 
 @app.route('/get_user', methods=['GET'])
 def get_user():
-    request_data = request.get_json()    
-    roll_no = request_data['roll_no']    
+    request_data = request.get_json()
+    roll_no = request_data['roll_no']
     results = es.get(index='index_student', id=roll_no)
-    return jsonify(results['_source']),200
+    return jsonify(results['_source']), 200
 
-# @app.route("/user",method=["GET"])
-# def fetch_all_user():
-#   results = es.index(index='index_student')
-#   return jsonify(results['_source']),200
 
 @app.route('/search_user', methods=['GET'])
 def search_user():
-    request_data = request.get_json()    
-    keyword = request_data['keyword']
-    # keyword = request.form['keyword']
+    request_data = request.get_json()
+    name = request_data['name']
+    roll_no = request_data["roll_no"]
+    
 
     query_body = {
         "query": {
-            "multi_match": {
-                "query": keyword,
-                "fields": ["name", "roll_no"]
+            "dis_max": {
+                "queries": [
+                    {"match": {"name": name}},
+                    {"match": {"roll_no": roll_no}}
+                ],
+                "tie_breaker": 0.3
             }
         }
     }
 
-    res = es.search(index="user", body=query_body)
-
+    res = es.search(index="index_student", body=query_body)
+    print(res)
     return jsonify(res['hits']['hits'])
